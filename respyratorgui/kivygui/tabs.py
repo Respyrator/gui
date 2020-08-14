1d26ba8f8567ec94144e3465feec4a18491fd8be
Cristobal Contreras <crisconru@gmail.com>
2020-08-14 14:34:40 +0200

    re do it tabs behaviour

--- respyratorgui/kivygui/tabs.py
+++ respyratorgui/kivygui/tabs.py
@@ -18,7 +18,6 @@
 # Installed -------------------------------------------------------------------
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, BooleanProperty, ObjectProperty
-from kivy.uix.togglebutton import ToggleButton
 # Coded -----------------------------------------------------------------------
 from . import logapp, load_kv
 # Program ---------------------------------------------------------------------
@@ -27,27 +26,39 @@ load_kv(__file__)
 
 
 class GuiTabs(BoxLayout):
+    tab_selected = ''
     tab_modes = ObjectProperty()
     tab_params = ObjectProperty()
     tab_alarms = ObjectProperty()
     tab_modes_blocked = BooleanProperty(False)
 
+    def _tab_clicked(self, tab: str, tab_obj):
+        self.parent.tab_clicked(tab)
+        if tab != self.tab_selected:
+            self.parent.tab_clicked(tab)
+        else:
+            tab_obj.state = 'down'
+
     def tab_modes_selected(self):
         self.tab_modes.state = 'down'
         self.tab_params.state = 'normal'
         self.tab_alarms.state = 'normal'
+        self.tab_selected = 'modes'
 
     def tab_params_selected(self):
         self.tab_modes.state = 'normal'
         self.tab_params.state = 'down'
         self.tab_alarms.state = 'normal'
+        self.tab_selected = 'params'
 
     def tab_alarms_selected(self):
         self.tab_modes.state = 'normal'
         self.tab_params.state = 'normal'
         self.tab_alarms.state = 'down'
+        self.tab_selected = 'alarms'
 
     def tab_nothing_selected(self):
         self.tab_modes.state = 'normal'
         self.tab_params.state = 'normal'
         self.tab_alarms.state = 'normal'
+        self.tab_selected = ''
